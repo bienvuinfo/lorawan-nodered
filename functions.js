5e31023ad98d39c1ddd5e97d3e11e3583fc1d0b6
bienvuinfo <info@bienvu.ch>
2023-01-15 22:40:54 +0100

    202301152240

--- functions.js
+++ functions.js
@@ -1,21 +1,31 @@
-let a="toat";
-function init(str){
-    
-console.log(str);    
-}
-
-function hex2dec(h){
-    c="0123456789ABCDEF";
-    t=0;
-    for (var i=0;i<h.length;i++){
-        t=t*16;
-        x=c.indexOf(h[i].toUpperCase());
-        t+=(x>0) ? x:0;
+function hex2dec(hexstream, bytes = 1) {
+  out = [];
+  hexstream = hexstream.split("");
+  c = "0123456789ABCDEF";
+  bytes = bytes < 1 ? 1 : bytes;
+  while (hexstream.length) {
+    bytes = bytes > hexstream.length ? hexstream.length : bytes;
+    chunk = hexstream.splice(0, bytes);
+    t = 0;
+    for (var i = 0; i < chunk.length; i++) {
+      t = t * 16;
+      x = c.indexOf(chunk[i].toUpperCase());
+      t += x > 0 ? x : 0;
     }
-    return t;
+    out.push(t);
+  }
+  return out;
 }
-function hexStreamToDecArray(str){
-    
 
+function dec2hex(d, p = 1) {
+  p = p < 1 ? 1 : p;
+  console.log(d);
+  out = "";
+  c = "0123456789ABCDEF".split("");
+  do {
+    x = d % 16;
+    out = c[x] + out;
+    d = parseInt(d / 16);
+  } while (d > 0);
+  return out.padStart(p, "0");
 }
-init("ABC");
