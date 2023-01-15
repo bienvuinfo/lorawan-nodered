67b68bb59dc647856aad2246c11b27f836b075b1
bienvuinfo <info@bienvu.ch>
2023-01-15 21:12:47 +0100

    202301152113

--- functions.js
+++ functions.js
@@ -1,1 +1,21 @@
-alert('hello');
+let a="toat";
+function init(str){
+    
+console.log(str);    
+}
+
+function hex2dec(h){
+    c="0123456789ABCDEF";
+    t=0;
+    for (var i=0;i<h.length;i++){
+        t=t*16;
+        x=c.indexOf(h[i].toUpperCase());
+        t+=(x>0) ? x:0;
+    }
+    return t;
+}
+function hexStreamToDecArray(str){
+    
+
+}
+init("ABC");
